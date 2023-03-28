8fffe74242fca2f1b1a33d3753c56e03184ad781
Sajeed Syed <123931227+mrsajids@users.noreply.github.com>
2023-03-28 14:55:34 +0530

    some

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let b = document.getElementById("btn");
-        let c = document.getElementsByClassName("listitem");
-        let ul = document.getElementsByClassName("ul");
-        
-        b.addEventListener('click', () => {
-            for(let i=0;i<c.length;i++){
-                c[i].classList.toggle("displayflex");
-            }
+let c = document.getElementsByClassName("listitem");
+let ul = document.getElementsByClassName("ul");
 
-            ul[0].classList.toggle("flexcolumn");
-            
-            if(ul[0].classList.contains("flexcolumn"))
-                ul[0].style.height="15rem";
-            else
-                ul[0].style.height="2.7rem";
-        });
+b.addEventListener('click', () => {
+    for (let i = 0; i < c.length; i++) {
+        c[i].classList.toggle("displayflex");
+    }
+
+    ul[0].classList.toggle("flexcolumn");
+
+    if (ul[0].classList.contains("flexcolumn"))
+        ul[0].style.height = "15rem";
+    else
+        ul[0].style.height = "2.7rem";
+});
+
+function hello() {
+    console.log("helo")
+}
