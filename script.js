b8d2235c4cbd3bd315dc535c3d2fe744b2925328
Sajeed Syed <123931227+mrsajids@users.noreply.github.com>
2023-03-30 17:13:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 let b = document.getElementById("btn");
+let can = document.getElementById("can");
 let c = document.getElementsByClassName("listitem");
 let ul = document.getElementsByClassName("ul");
+let windowwidth = window.innerWidth;
 
 b.addEventListener('click', () => {
-    for (let i = 0; i < c.length; i++) {
-        c[i].classList.toggle("displayflex");
-    }
+  b.style.display = "none";
+  can.style.display = "block";
+  for (let i = 0; i < c.length; i++) {
+    c[i].classList.add("displayflex");
+  }
 
-    ul[0].classList.toggle("flexcolumn");
+  ul[0].classList.add("flexcolumn");
 
-    if (ul[0].classList.contains("flexcolumn"))
-        ul[0].style.height = "15rem";
-    else
-        ul[0].style.height = "2.7rem";
+  if (ul[0].classList.contains("flexcolumn"))
+    ul[0].style.height = "15rem";
+  else
+    ul[0].style.height = "2.7rem";
 });
+can.addEventListener('click', () => {
+  cls();
+});
+function cls() {
+  if (windowwidth < 710) {
+    b.style.display = "block";
+  }
+  can.style.display = "none";
+  for (let i = 0; i < c.length; i++) {
+    c[i].classList.remove("displayflex");
+  }
 
-function hello() {
-    console.log("helo")
+  ul[0].classList.remove("flexcolumn");
+  if (ul[0].classList.contains("flexcolumn"))
+    ul[0].style.height = "15rem";
+  else
+    ul[0].style.height = "4rem";
 }
